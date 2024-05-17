460bc80376738a5385db82bf133b9da81fd7d377
arbiterj <jaysonpmercurio@gmail.com>
2024-05-16 22:29:13 -0700

    arduino

---
+++ ergoduino/ergoduino.ino
@@ -0,0 +1,43 @@
+#include <Joystick.h>
+
+#include <HID_Buttons.h>
+
+/*Resources
+https://github.com/dmadison/HID_Buttons
+https://github.com/MHeironimus/ArduinoJoystickLibrary
+*/
+
+// Using Arduino Micro
+/***PINOUT***/
+/*
+https://content.arduino.cc/assets/Pinout-Micro_latest.png
+Left joystick y A0
+Left joystick x A1
+Right joystick y A2
+Right joystick x A3
+START A4/D22
+X A5/D23
+L3 CIPO/D14
+R3 SCK/D15
+Y COPI/D16
+A SS/D17
+B D1/TX
+DRIGHT D0/RX
+DDOWN D2/SDA
+DLEFT D3
+DUP D4
+SELECT D5
+LShoulder D6
+LTrigger D7
+RShoulder D8
+RTrigger D9
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
