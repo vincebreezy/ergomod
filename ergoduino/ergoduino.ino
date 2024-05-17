bd08ce2076c762c69f1cfcb615292d5e7429d9d4
Andrew Chau <andrew.chau@sjsu.edu>
2024-05-17 01:39:54 -0700

    First implementation for arduino

--- ergoduino/ergoduino.ino
+++ ergoduino/ergoduino.ino
@@ -1,5 +1,4 @@
 #include <Joystick.h>
-
 #include <HID_Buttons.h>
 
 /*Resources
@@ -32,12 +31,75 @@ LTrigger D7
 RShoulder D8
 RTrigger D9
 */
-void setup() {
-  // put your setup code here, to run once:
 
+// pin declarations
+const int leftjoy_y = A0;
+const int leftjoy_x = A1;
+const int rightjoy_y = A2;
+const int rightjoy_x = A3;
+const int buttonPins[] = {22, 23, 14, 15, 16, 17, 1, 0, 2, 3, 4, 5, 6, 7, 8, 9}
+
+// create the joystick
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
+                   JOYSTICK_TYPE_JOYSTICK, 16, 0,           // 16 button count 
+                   true, true, false, false, false, false,  // x, y axes true
+                   false, false, false, false, false);         
+
+// to manage button states
+JoystickButton buttons[16] = {
+    JoystickButton(0), JoystickButton(1), JoystickButton(2), JoystickButton(3),
+    JoystickButton(4), JoystickButton(5), JoystickButton(6), JoystickButton(7),
+    JoystickButton(8), JoystickButton(9), JoystickButton(10), JoystickButton(11),
+    JoystickButton(12), JoystickButton(13), JoystickButton(14), JoystickButton(15)
+};
+
+void setup() {
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);  
+
+  Joystick.setXAxisRange(0, 1023);
+  Joystick.setYAxisRange(0, 1023);
+  Joystick.setRxAxisRange(0, 1023);
+  Joystick.setRyAxisRange(0, 1023);
+
+  for (int i = 0; i < 16; i++) {
+      pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+
+  Joystick.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int leftY_data = analogRead(leftjoy_y);
+  int leftX_data = analogRead(leftjoy_x);
+  int rightY_data = analogRead(rightjoy_y);
+  int rightX_data = analogRead(rightjoy_x);
 
+  Joystick.setXAxis(leftY_data);
+  Joystick.setYAxis(leftX_data);
+  Joystick.setRxAxis(rightY_data);
+  Joystick.setRyAxis(rightX_data);
+
+  for (int i = 0; i < 16; i++) {
+    buttons[i].set(!digitalRead(buttonPins[i]));
+  }
+
+  Joystick.sendState();
+
+  // manual debug print
+  // Serial.print("leftY: ");
+  // Serial.println(leftY_data);
+  // Serial.print("\t");
+  // Serial.print("leftX: ");
+  // Serial.println(leftX_data);
+  // Serial.print("\t");
+  // Serial.print("rightY: ");
+  // Serial.println(rightY_data);
+  // Serial.print("\t");
+  // Serial.print("rightX: ");
+  // Serial.println(rightX_data);  
+
+  delay(50); // ms
 }
